8c59ba78a45ef7f0c344509f53ac0855063fbe36
Sean B <seanb4@hotmail.co.uk>
2024-10-31 10:18:07 +0000

    updated database

--- backend/database.js
+++ backend/database.js
@@ -2,7 +2,8 @@ const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
 // Create a new database file if it doesn't exist
-const dbPath = path.join(__dirname, 'database.sqlite');
+//const dbPath = path.join(__dirname, 'database.sqlite');
+const dbPath = process.env.DATABASE_URL || path.join(__dirname, 'data', 'database.sqlite');
 const db = new sqlite3.Database(dbPath, (err) => {
   if (err) {
     console.error('Error opening database ' + err.message);
